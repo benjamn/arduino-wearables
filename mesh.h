2c654d6ce44b423625da71311c192278f5052ed2
Ben Newman <ben@benjamn.com>
2019-07-24 18:37:15 -0400

    Simplify mesh.h to remove boilerplate example code.

--- mesh.h
+++ mesh.h
@@ -9,41 +9,9 @@
 painlessMesh mesh;
 Scheduler userScheduler;
 
-void sendMessage();
-Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
-void sendMessage() {
-  String msg = "Hello from node ";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast(msg);
-  taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
-}
-
-void receivedCallback(uint32_t from, String &msg) {
-  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
-}
-
-void newConnectionCallback(uint32_t nodeId) {
-  Serial.printf("--> New Connection, nodeId = %u\n", nodeId);
-}
-
-void changedConnectionCallback() {
-  Serial.printf("Changed connections %s\n", mesh.subConnectionJson().c_str());
-}
-
-void nodeTimeAdjustedCallback(int32_t offset) {
-  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
-}
-
 void meshSetup() {
   mesh.setDebugMsgTypes(ERROR | STARTUP);
   mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-
-  userScheduler.addTask(taskSendMessage);
-  taskSendMessage.enable();
 }
 
 uint32_t lastTime = 0;
@@ -51,12 +19,6 @@ uint32_t lastTime = 0;
 void meshLoop() {
   userScheduler.execute();
   mesh.update();
-
-  uint32_t now = mesh.getNodeTime();
-  if (now - lastTime > 1000000) {
-    lastTime = now;
-    Serial.printf("Time: %d\n", now);
-  }
 }
 
 uint32_t meshTimeMs() {
