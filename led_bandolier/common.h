2eba657c079f1c280aa477b829d2cb9a52bd4916
Benedict Hsieh <ben.hsieh@gmail.com>
2019-07-26 21:25:08 -0400

    synchronize pulse when meshed and small refactor
    - add a pulse behavior when meshed
    - refactor some animation functions to work on an RGB byte array instead of
    returning an int.

--- led_bandolier/common.h
+++ led_bandolier/common.h
@@ -9,15 +9,18 @@ const uint8_t inputPin = 14;
 
 Adafruit_WS2801 strip = Adafruit_WS2801(NUM_PIXELS, dataPin, clockPin);
 
-int8_t dimnessFactors[] = {
-  1, // full brightness
+int8_t switchDimnessFactors[] = {
+  // disable full brightness
+  // 1, // full brightness
   2, // half
   8, // eighth
  -1, // off
 };
 
+byte MAX_BRIGHT = 255;
+
 size_t dimnessIndex = 0;
-size_t dimnessCount = sizeof(dimnessFactors) / sizeof(dimnessFactors[0]);
+size_t dimnessCount = sizeof(switchDimnessFactors) / sizeof(switchDimnessFactors[0]);
 
 void changeDimness() {
   // TODO Turn off the WiFi radio when dimnessFactor is -1.
@@ -25,25 +28,36 @@ void changeDimness() {
 }
 
 // Create a 24 bit color value from R,G,B
-uint32_t Color(byte r, byte g, byte b)
+uint32_t Color(byte rgb[], byte brightness)
 {
-  size_t dimnessFactor = dimnessFactors[dimnessIndex];
-  if (dimnessFactor < 0) {
+  size_t switchDimnessFactor = switchDimnessFactors[dimnessIndex];
+  if (switchDimnessFactor < 0) {
     return 0;
   }
+
+  byte r = rgb[0];
+  byte g = rgb[1];
+  byte b = rgb[2];
+  
   uint32_t c;
-  c = r / dimnessFactor;
+  c = r / switchDimnessFactor * brightness / MAX_BRIGHT;
   c <<= 8;
-  c |= g / dimnessFactor;
+  c |= g / switchDimnessFactor * brightness / MAX_BRIGHT;
   c <<= 8;
-  c |= b / dimnessFactor;
+  c |= b / switchDimnessFactor * brightness / MAX_BRIGHT;
   return c;
+}
+
+void setRGB(byte r, byte g, byte b, byte rgb[]) {
+  rgb[0] = r;
+  rgb[1] = b;
+  rgb[2] = g;
 }
 
 void clear() {
   for (int i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, Color(0, 0, 0));
+    byte rgb[] = {0, 0, 0};
+    strip.setPixelColor(i, Color(rgb, 0));
   }
   strip.show();
 }
-
