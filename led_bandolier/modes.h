5f6f1bf93505e3852c34ae8436bd644138bc348c
Benedict Hsieh <ben.hsieh@gmail.com>
2019-07-30 16:32:33 -0400

    oh no, compile order problems

--- led_bandolier/modes.h
+++ led_bandolier/modes.h
@@ -11,6 +11,16 @@
 #include "green.h"
 #include "blue.h"
 
+void setScheme(byte colors[][3], ColorScheme& scheme) {
+  int numColors = sizeof(colors) / sizeof(byte[3]);
+  scheme.numColors = numColors;
+  for (int i = 0; i < numColors; i++) {
+    for (int j = 0; j < 3; j++) {
+      scheme.colors[i][j] = colors[i][j];
+    }
+  }
+}
+
 void (*modes[])(AnimationState*, byte[3]) = {
   rainbows,
   chasers,
@@ -19,15 +29,53 @@ void (*modes[])(AnimationState*, byte[3]) = {
   solidBlue,
 };
 
+// Parallel array to the above
+bool modeSupportsSchemes = {
+  false,
+  true,
+  false,
+  false,
+  false
+};
+
 byte modeCount = sizeof(modes) / sizeof(modes[0]);
 byte modeIndex = 0;
 
+// Unfortunately these schemes are initialized at runtime in modeSetup() because
+// "nontrivial initializers are not implemented".
+ColorScheme schemes[3];
+byte schemeCount = sizeof(schemes) / sizeof(schemes[0]);
+byte schemeIndex = 0;
+byte jointCount = schemeCount * modeCount;
+
+void colorSchemesSetup() {
+  // Initialize the color schemes
+    
+  // Scheme 0: red, green, and blue
+  byte scheme0[][3] = {{255, 0, 0}, {0, 255, 0}, {0, 0, 255}};
+  setScheme(scheme1, schemes[0]);
+
+  // Scheme 1: purple and pink
+  byte scheme1[][3] = {{215, 0, 130}, {110, 0, 210}};
+  setScheme(scheme2, schemes[1]);
+
+  // Scheme 2: orange-red and light blue
+  byte scheme2[][3] = {{255, 84, 10}, {10, 190, 255}};
+  setScheme(scheme2, schemes[2]);
+}
+
+ColorScheme& currentScheme() {
+  return schemes[schemeIndex];
+}
+
 void modeSetup() {
   EEPROM.begin(EEPROM_SIZE);
-  modeIndex = EEPROM.read(EEPROM_ADDR);
-  if (modeIndex >= modeCount) {
-    modeIndex = 0;
+  byte jointIndex = EEPROM.read(EEPROM_ADDR);
+  if (jointIndex >= jointCount) {
+    jointIndex = 0;
   }
+  modeIndex = jointIndex / modeCount;
+  schemeIndex = jointIndex % modeCount;
 }
 
 bool needToCommit = false;
@@ -35,8 +83,20 @@ uint32_t timeOfLastCommit = 0;
 uint32_t commitDelayMs = 10000;
 
 void changeMode() {
-  modeIndex = (modeIndex + 1) % modeCount;
-  EEPROM.write(EEPROM_ADDR, modeIndex);
+  // Within one mode, iterate through color schemes.
+  // If out of color schemes, move onto the next mode.
+  if (modeSupportsSchemes[modeIndex]) {
+    schemeIndex = (schemeIndex + 1) % schemeCount;
+    if (schemeIndex == 0) {
+      modeIndex = (modeIndex + 1) % modeCount;
+    }
+  } else {
+    // If the mode doesn't support schemes, just move onto the next mode
+    modeIndex = (modeIndex + 1) % modeCount;
+  }
+
+  byte jointIndex = modeIndex * schemeCount + schemeIndex;
+  EEPROM.write(EEPROM_ADDR, jointIndex);
   needToCommit = true;
 }
 
