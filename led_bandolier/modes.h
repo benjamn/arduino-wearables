19f4e304e8c7333b4fe25c239b799518b3e33ccb
Benedict Hsieh <ben.hsieh@gmail.com>
2019-07-30 16:36:56 -0400

    compiles now but dependencies are messed up

--- led_bandolier/modes.h
+++ led_bandolier/modes.h
@@ -11,6 +11,13 @@
 #include "green.h"
 #include "blue.h"
 
+typedef struct {
+  // Maximum of 4 colors, each is 3 bytes rgb
+  byte colors[4][3];
+  byte numColors;
+} ColorScheme;
+
+
 void setScheme(byte colors[][3], ColorScheme& scheme) {
   int numColors = sizeof(colors) / sizeof(byte[3]);
   scheme.numColors = numColors;
@@ -30,7 +37,7 @@ void (*modes[])(AnimationState*, byte[3]) = {
 };
 
 // Parallel array to the above
-bool modeSupportsSchemes = {
+bool modeSupportsSchemes[] = {
   false,
   true,
   false,
@@ -53,11 +60,11 @@ void colorSchemesSetup() {
     
   // Scheme 0: red, green, and blue
   byte scheme0[][3] = {{255, 0, 0}, {0, 255, 0}, {0, 0, 255}};
-  setScheme(scheme1, schemes[0]);
+  setScheme(scheme0, schemes[0]);
 
   // Scheme 1: purple and pink
   byte scheme1[][3] = {{215, 0, 130}, {110, 0, 210}};
-  setScheme(scheme2, schemes[1]);
+  setScheme(scheme1, schemes[1]);
 
   // Scheme 2: orange-red and light blue
   byte scheme2[][3] = {{255, 84, 10}, {10, 190, 255}};
